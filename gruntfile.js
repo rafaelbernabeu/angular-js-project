f82e3b95eff92ab86faa92c82e1dd4d069ca76ea
Rafael <rbbernabeu@gmail.com>
2018-12-23 17:17:30 -0200

    change hostname to correct access via docker exposed port

--- gruntfile.js
+++ gruntfile.js
@@ -109,7 +109,7 @@ module.exports = function (grunt) {
     connect: {
       options: {
         port: 9002,
-        hostname: 'localhost',
+        hostname: '*',
       },
       livereload: {
         options: {
